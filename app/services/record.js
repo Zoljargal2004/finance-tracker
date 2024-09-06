c1478c3fde8c09505e25d8cf4fc11a03da07272c
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-09-06 14:40:51 +0800

    last day i assume

--- app/services/record.js
+++ app/services/record.js
@@ -1,20 +1,44 @@
-export const addRecord = async (name, amount, type, description, categoryID, date, time) => {
-  try{const res = fetch(`http://localhost:4000/record/add`, {
-    method: "POST",
-    headers: { "Content-type": "application/json; charset=UTF-8" },
-    body: JSON.stringify({
-      name: name,
-      amount: amount,
-      type: type,
-      description: description,
-      categoryID: categoryID,
-      date: date,
-      time: time
-    }),
-  });}
-  catch(error){
-    console.log(error)
+export const addRecord = async (
+  name,
+  amount,
+  type,
+  description,
+  categoryID,
+  date,
+  time
+) => {
+  try {
+    const res = fetch(`http://localhost:4000/record/add`, {
+      method: "POST",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({
+        name: name,
+        amount: amount,
+        type: type,
+        description: description,
+        categoryID: categoryID,
+        date: date,
+        time: time,
+      }),
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
 
+export const getRecords = async (date) => {
+  try {
+    const res = await fetch(`http://localhost:4000/record/list`, {
+      method: "GET",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({
+        date: date,
+      }),
+    });
 
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- components/chunks/heroChunks/hero.js
+++ components/chunks/heroChunks/hero.js
@@ -1,9 +1,67 @@
-import { SimpleTable } from "./WHATEVERITIS";
+"use client";
+
+import { icons } from "@/app/data/icons";
+import { getRecords } from "@/app/services/record";
+import { Checkbox } from "@/components/ui/checkbox";
+import { getDate } from "date-fns";
+import { useEffect, useState } from "react";
 
 const Records = () => {
   return (
-    <div>
-      <SimpleTable />
+    <div className="w-full">
+      <TheDay />
+    </div>
+  );
+};
+
+const CustomCard = () => {
+  const TheIcon = icons[0].Icon;
+  const color = "#0166FF";
+  const name = "Lending and  Renting";
+  const time = "14:00";
+  const amount = 1000;
+  const type = "INCOME";
+
+  return (
+    <div className="rounded-[12px] w-full flex justify-between items-center border-[#E5E7EB] border-[1px] py-3 px-6">
+      <div className="flex gap-4 items-center">
+        <Checkbox className={`size-6`} />
+        <div className="size-10 bg-[#0166FF] content-center rounded-full">
+          <TheIcon size={24} className="rounded-full m-auto text-white" />
+        </div>
+        <div className="flex flex-col">
+          <span className="font-semibold">{name}</span>
+          <span className="text-[#6B7280] text-[12px]">{time}</span>
+        </div>
+      </div>
+      <span
+        className={
+          (amount == "EXPENSE" && "text-[#F54949]") || "text-[#23E01F]"
+        }
+      >
+        {(type == "EXPENSE" && "-") || "+"}
+        {amount}
+      </span>
+    </div>
+  );
+};
+
+const load = () => {};
+
+const TheDay = async () => {
+  const [list, setList] = useState([]);
+  const day = "2024-9-4";
+  useEffect(() => {
+    async () => {
+      setList(await getRecords(day));
+    };
+  }, []);
+  console.log(list);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <span className="">{day}</span>
+      <CustomCard />
     </div>
   );
 };
